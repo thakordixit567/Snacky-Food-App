8e70f9c925fdeb15ae79f5b89a6cb47c516d0f7e
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-05-30 19:55:41 +0530

    UPDATE!

--- snacky-client/src/components/Banner.jsx
+++ snacky-client/src/components/Banner.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
 
 const Banner = () => {
   return (
-    <div>Banner</div>
-  )
-}
+    <div className="section-container bg-gradient-to-r from-[#FAFAFA] from-0% to-[#FCFCFC] to-100%">
+     <div className="py-24 flex flex-col md:flex-row justify-between items-center gap-8">
 
-export default Banner
+          {/*texts*/}
+     <div className="md:w-1/2 space-y-7 ">
+      <h2 className="md:text-5xl text-4xl font-bold md:leading-snug leading-snug">
+      Explore the Exquisite Enjoyment of  <span className="text-orange">Food.</span></h2>
+      <p className="text-xl text-[#4A4A4A]">
+      Discover a world where every plate tells a tale of culinary expertise and dedicated artistry. 
+      </p>
+      <button className="btn bg-orange px-8 py-3 font-semibold text-white rounded-full">Order Now</button>
+     </div>
+
+          {/* Images */}
+     <div className="md:w-1/2">Right</div>
+     </div>
+    </div>
+  );
+};
+
+export default Banner;
