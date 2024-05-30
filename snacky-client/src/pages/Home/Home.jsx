934810a362750a47db4f3c637527676c181899dd
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-05-30 10:40:43 +0530

    UPDATE!

--- snacky-client/src/pages/Home/Home.jsx
+++ snacky-client/src/pages/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Banner from '../../components/Banner'
+
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='max-w-screen-2xl container mx-auto xl:px-24 px-4'>
+    <Banner/>
+    </div>
   )
 }
 
