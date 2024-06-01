7698245eaf67274656abe5a42b87307a8a4cdc4c
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-01 19:27:01 +0530

    UPDATE!

--- snacky-client/src/components/Navbar.jsx
+++ snacky-client/src/components/Navbar.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "/logo.png";
 import { FiPhoneCall } from "react-icons/fi";
 
 const Navbar = () => {
+  const [isSticky, setIsSticky] = useState(false);
+
+  //handle scroll facility
+  useEffect(() => {
+    const handleScroll = () => {
+      const offset = window.screenY;
+      if (offset > 0) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.addEventListener("scroll", handleScroll);
+    };
+
+  }, []);
+
   const navItems = (
     <>
       <li>
@@ -74,11 +95,11 @@ const Navbar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-             {navItems}
-             </ul>
+              {navItems}
+            </ul>
           </div>
-          <a href="/" >
-            <img src={Logo}  alt="" />
+          <a href="/">
+            <img src={Logo} alt="" />
           </a>
         </div>
         <div className="navbar-center hidden lg:flex">
@@ -124,7 +145,9 @@ const Navbar = () => {
                   d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
                 />
               </svg>
-              <span className="badge bg-green text-white badge-sm indicator-item">8</span>
+              <span className="badge bg-green text-white badge-sm indicator-item">
+                8
+              </span>
             </div>
           </div>
 
