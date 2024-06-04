f71313599857a9149f16c9274de8f0bd24d9581b
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-04 09:36:22 +0530

    UPDATE!

--- snacky-client/src/layout/Mainlayout.jsx
+++ snacky-client/src/layout/Mainlayout.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import "../App.css"
+import SpecialDishes from '../pages/Home/SpecialDishes'
 
 const Mainlayout = () => {
   return (
     <div >
       <Navbar/>
      <Outlet/>
-
+     <SpecialDishes/> 
     </div>
   )
 }
--- snacky-client/src/pages/Home/Categories.jsx
+++ snacky-client/src/pages/Home/Categories.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
-
+const categoryItems = [
+  {id: 1, title: "Main Dish", des:"(86 dishes)", image: "/images/Home/category/img1.png"},
+  {id: 2, title: "Break Fast", des:"(12 break fast)", image: "/images/Home/category/img2.png"},
+  {id: 3, title: "Dessert", des:"(48 dessert)", image: "/images/Home/category/img3.png"},
+  {id: 4, title: "Browse All", des:"(255 Items)", image: "/images/Home/category/img4.png" }
+]
 
 const Categories = () => {
   return (
@@ -13,7 +18,23 @@ const Categories = () => {
          </div>
 
          {/* Category Card */}
-         <div></div>
+         <div className='flex flex-col sm:flex-row flex-wrap gap-8 justify-around items-center mt-12'>
+           {
+             categoryItems.map((item, i) => (
+               <div key={i} 
+               className='shadow-lg rounded-md bg-white py-6 px-5 w-72 mx-auto text-center cursor-pointer
+               hover:-translate-y-4 duration-300 transition-all'>
+                  <div className='flex w-full mx-auto items-center justify-center'>
+                    <img src={item.image} alt="" className='bg-[#C1F1C6] p-5 rounded-full w-28 h-28' />
+                  </div>
+                  <div className='mt-5 space-y-1'>
+                    <h5>{item.title}</h5>
+                    <p>{item.des}</p>
+                  </div>
+               </div>
+             ))
+           }
+         </div>
     </div>
   )
 }
