8efbf3fc3f04d40b895c806e5fed4e8c7cef1578
thakordixit567 <103193972+thakordixit567@users.noreply.github.com>
2024-06-05 09:56:18 +0530

    UPDATE!

---
+++ snacky-client/src/components/Cards.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaHeart } from "react-icons/fa";
+
+const Cards = ({ item }) => {
+  const [isHeartfill, setIsHeartfill] = useState(false);
+
+  const handleheartfill = () => {
+    setIsHeartfill(!isHeartfill);
+  };
+  return (
+    <div className="card w-96 bg-gradient-to-r from-slate-200 to-gray-200 shadow-xl relative">
+      <div 
+      className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-green ${isHeartfill ?  
+        "text-rose-500" : "text-white"}`}
+       onClick={handleheartfill} 
+        >
+       <FaHeart className="h-5 w-5 cursor-pointer"/>
+      </div>
+      <Link to={`/menu/${item._id}`}>
+        <figure>
+          <img
+            src={item.image}
+            className="hover:scale-105 transition-all duration-200 md:h-72"
+            alt=""
+          />
+        </figure>
+      </Link>
+      <div className="card-body">
+        <h2 className="card-title">{item.name}</h2>
+        <p>{item.recipe}</p>
+        <div className="card-actions justify-between items-center mt-2">
+          <h5 className="font-semibold">
+            <span className="text-sm text-red">₹</span> {item.price}
+          </h5>
+          <button className="btn bg-orange text-white ">Buy Now</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cards;
--- snacky-client/src/pages/Home/SpecialDishes.jsx
+++ snacky-client/src/pages/Home/SpecialDishes.jsx
@@ -1,16 +1,73 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Cards from "../../components/Cards";
 
 const SpecialDishes = () => {
-  return (
-    <div className='section-container my-20'>
-    <div className='text-left'>
-    <p className='subtitle'>Special Dishes</p>
-      <h2 className='title md:w-96'>
-      Highlighted Menu Items.
-      </h2>
-    </div>
-    </div>
-  )
-}
+  const [recipes, setRecipes] = useState([]);
+  const slider = React.useRef(null);
 
-export default SpecialDishes
+  useEffect(() => {
+    fetch("/menu.json")
+      .then((res) => res.json())
+      .then((data) => {
+        const specialpro = data.filter((item) => item.category === "popular");
+        //console.log(specialpro)
+        setRecipes(specialpro);
+      });
+  }, []);
+
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+  return (
+    <div className="section-container my-20">
+      <div className="text-left">
+        <p className="subtitle">Special Dishes</p>
+        <h2 className="title md:w-[520px]">Highlighted Menu Items.</h2>
+      </div>
+
+      <Slider {...settings}>
+        {
+          recipes.map((item,i) => (
+            <Cards key={i} item={item} />
+          ))
+        }
+      </Slider>
+    </div>
+  );
+};
+
+export default SpecialDishes;
